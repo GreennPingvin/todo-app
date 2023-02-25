178b571666a2682a3e1fec9c74f85d573d426ebb
redo_oreon <vladsok1994@gmail.com>
2023-02-25 14:23:13 +0300

    add icon to Todo

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,9 +1,11 @@
+import { RiTodoFill } from 'react-icons/ri'
 import styles from './Todo.module.css'
 
 export default function Todo(props) {
   const { todo, onDoubleClick } = props
   return (
     <div className={styles.todo} onDoubleClick={onDoubleClick}>
+      <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo}</div>
     </div>
   )
