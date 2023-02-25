9b5e27d69c102df530f714e34dede9d19ee717de
redo_oreon <vladsok1994@gmail.com>
2023-02-25 12:22:51 +0300

    add statement 'Todo list is empty'

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -5,6 +5,7 @@ export default function TodoList(props) {
   const { todoList } = props
   return (
     <div className={styles.todoListContainer}>
+      {!todoList.length && <h2>Todo list is empty</h2>}
       {todoList.map((todo, i) => (
         <Todo todo={todo} key={i} />
       ))}
