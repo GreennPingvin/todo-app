31b215fb3cb4592c4a113a01c4fcae1d61a16381
redo_oreon <vladsok1994@gmail.com>
2023-02-25 12:03:18 +0300

    import styles to Todo and TodoList

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,8 +1,10 @@
+import styles from './Todo.module.css'
+
 export default function Todo(props) {
   const { todo } = props
   return (
-    <>
-      <h2>{todo}</h2>
-    </>
+    <div className={styles.todo}>
+      <div className={styles.todoText}>{todo}</div>
+    </div>
   )
 }
--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,9 +1,10 @@
 import Todo from './Todo'
+import styles from './TodoList.module.css'
 
 export default function TodoList(props) {
   const { todoList } = props
   return (
-    <div>
+    <div className={styles.todoListContainer}>
       {todoList.map((todo, i) => (
         <Todo todo={todo} key={i} />
       ))}
