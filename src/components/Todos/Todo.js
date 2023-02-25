8f4d3687f70f13565a05ee695c378c9c9a469dfa
redo_oreon <vladsok1994@gmail.com>
2023-02-25 15:30:17 +0300

    create todo object and save as a state of App

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -2,11 +2,15 @@ import { RiTodoFill } from 'react-icons/ri'
 import styles from './Todo.module.css'
 
 export default function Todo(props) {
-  const { todo, onDoubleClick } = props
+  const { id, todo, onDoubleClick } = props
   return (
-    <div className={styles.todo} onDoubleClick={onDoubleClick}>
+    <div
+      id={id}
+      className={styles.todo}
+      onDoubleClick={() => onDoubleClick(id)}
+    >
       <RiTodoFill className={styles.todoIcon} />
-      <div className={styles.todoText}>{todo}</div>
+      <div className={styles.todoText}>{todo.text}</div>
     </div>
   )
 }
