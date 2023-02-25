3e9d3b1712256acdfae719557f2580a4e06a462e
redo_oreon <vladsok1994@gmail.com>
2023-02-25 13:21:04 +0300

    add statement when there is no text in the input

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -4,23 +4,30 @@ import styles from './TodoForm.module.css'
 export default function TodoForm(props) {
   const { addTodo } = props
   const [text, setText] = useState('')
+  const [warning, setWarning] = useState(false)
 
   const onSubmitHandler = (e) => {
     e.preventDefault()
-    addTodo(text)
-    setText('')
+    if (text) {
+      addTodo(text)
+      setText('')
+      setWarning(false)
+    } else {
+      setWarning(true)
+    }
   }
   return (
     <div className={styles.todoFormContainer}>
       <form onSubmit={onSubmitHandler}>
         <input
           type="text"
-          placeholder="Enter new todo"
+          placeholder={'Enter new todo'}
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
         <button type="submit">Submit</button>
       </form>
+      {warning && <div>Enter new todo</div>}
     </div>
   )
 }
